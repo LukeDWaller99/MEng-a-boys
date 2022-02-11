64ba408a11ea410cb3fb19f8a67b0e236feac958
LukeDWaller99 <lukedwaller99@gmail.com>
2022-02-11 16:05:26 +0000

    Update main.cpp

--- Controller/main.cpp
+++ Controller/main.cpp
@@ -9,19 +9,24 @@
 
 // for initial test, map the pot value to the led strip on the MSB as a test
 // need to create an analog in instance of the pot 
+
+// Test Code setup for dummy operation on the UoP MSB
+PortOut LEDs(PortE);
 AnalogIn pot(PA_0);
 int potVal;
 int eightBitPotVal;
 int quantisedVal;
-PortOut LEDs(PortE);
-DigitalIn Btn_A(PG_0), Btn_B(PG_1), Btn_C(PG_2), Btn_D(PG_3);
-
-
 int redOn = 4096; //    0b0001000000000000;
 int greenOn = 8192; //  0b0010000000000000;
 int blueOn = 16384; //  0b0100000000000000;
 int ledMask = 1020; //  0b0000001111111100;
 int allOn =  0b0111000000000000;
+
+// DigitalIn Btn_1(), Btn_2(), Btn_3(), Btn_4(), Btn_L3(), Btn_R3();
+// DigitalOut buzz(), led_1(), led_2();
+// AnalogIn L_Pitch(), L_Roll(), R_Pitch(), R_Roll(), Pot_1(), Pot_2();
+// nRF24L01P Serial_Comms();
+
 
 int quantisePotVal (int EightBitPotVal){
     int LedMask = 0;
@@ -47,8 +52,6 @@ int quantisePotVal (int EightBitPotVal){
 
 // create an array of outputs for the leds for the output
 int main() {
-    
-
     while (true) {
     LEDs = ledMask | allOn;
     potVal = pot.read_u16();
