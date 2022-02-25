37600b196d9789f88eb1978f15c4fdc62f34e9c1
LukeDWaller99 <lukedwaller99@gmail.com>
2022-02-25 18:21:09 +0000

    Finalise Circuit Schematic

--- Controller/HARDWARE.h
+++ Controller/HARDWARE.h
@@ -4,12 +4,12 @@
 #include <PinNames.h>
 
 // pins for nRF24L01
-#define MOSI    PA_12   // D2  - good 
-#define MISO    PA_11   // D10 - good 
-#define SCK     PB_3    // D13 - good 
-#define CSN     PB_4    // D12 - good 
-#define CE      PB_5    // D11 - good
-#define IRQ     PA_8    // D9  - good 
+#define MOSI    PA_12   // D2
+#define MISO    PA_11   // D10
+#define SCK     PB_3    // D13
+#define CSN     PB_4    // D12
+#define CE      PB_5    // D11
+#define IRQ     PA_8    // D9 
 
 // joystick pins
 #define L_PITCH PA_0    // A0
@@ -18,18 +18,18 @@
 #define R_ROLL  PA_5    // A4
 
 // input buttons
-#define BTN1    PA_9 // D1 - good
-#define BTN2    PB_1 // D6 - good
-#define BTN3    PA_3 // A2 - good
+#define BTN1    PA_9 // D1
+#define BTN2    PB_1 // D6
+#define BTN3    PA_3 // A2
 
 // //input switches
-#define SW1     PB_0  // D3 - good  
+#define SW1     PB_0  // D3 
 
 // leds
-#define LED_1   PA_10 // D0 - good 
+#define LED_1   PA_10 // D0
 
 
 // buzzer
-#define BUZZER  PB_6 // D5 - good
+#define BUZZER  PB_6 // D5
 
 #endif
