02e14062f6eb96a1796e381c505b20cf31f98e7a
LukeDWaller99 <lukedwaller99@gmail.com>
2022-05-07 15:11:11 +0100

    Finish Debugging Code for controller

--- Hardware Debugging/Controller/main.cpp
+++ Hardware Debugging/Controller/main.cpp
@@ -1,74 +1,79 @@
 /**
     @file Main.cpp
-    Main file for Final Year Project - Luke Waller
+    Main debugging file for controller board Final Year Project - Luke Waller
 **/
-
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
 #include <mbed.h>
-#include <nRF24L01P.h>
-#include <string>
+#include <DigitalIn.h>
+#include <DigitalOut.h>
+#include <ThisThread.h>
+
 #include "Buzzer.h"
-#include "Callback.h"
-#include "DigitalIn.h"
-#include "DigitalOut.h"
-#include "EventQueue.h"
-#include "PinNames.h"
-#include "ThisThread.h"
 #include "HARDWARE.h"
-#include "TRANSMISSION_CODES.h"
-
-#define TRANSFER_SIZE   5
-#define DEFAULT_PIPE    0 // set the defauly pipe for the nRF24L01
-
-#define MULTIPLYING_FACTOR  2.0f
-#define POT_OFFSET          1.0f
-
-#define RADIO_QUEUE_LENGTH 32
-
-
-// MOSI, MISO, SCK, CNS, CE, IRQ - must be an interrupt pin 6 
-// nRF24L01P nRF24L01(MOSI, MISO, SCK, CSN, CE, IRQ);
 
 DigitalIn BT1(BTN1), BT2(BTN2), BT3(BTN3), SW_1(SW1);        
 DigitalOut LED(LED_1);                                             
 AnalogIn L_Pitch(L_PITCH), L_Roll(L_ROLL), R_Pitch(R_PITCH), R_Roll(R_ROLL);   
 Buzzer buzzer(BUZZER);
 
+Thread btnThread, swThread, potThread; // creating threads
 
+Mutex potMutex; // creating mutex lock for pots
 
-// create an array of outputs for the leds for the output
+void potMethod();
+void btnMethod();
+void swMethod();
+
 int main() {
 
     printf("Starting Board...\n");
 
-    LED = 1;
     buzzer.chime();
 
-    float leftPitch, rightPitch, leftRoll, rightRoll ;
+    // starting Threads
+    potThread.start(potMethod);
+    btnThread.start(btnMethod);
+    swThread.start(swMethod);
+
+}
+
+void swMethod(){
+    while (true) {
+        LED = SW_1 == 1 ? 1 : 0;
+    }
+}
+
+void btnMethod(){
+    while (true) {
+        buzzer = BT1 == 1 || BT2 == 1 || BT3 == 1 ? 1 : 0;
+    }
+}
+
+void potMethod(){
+
+    float leftPitch, rightPitch, leftRoll, rightRoll;
 
     while (true) {
-
-        if (SW_1 == 1){
-            LED = 1;
-        } else {
-            LED = 0 ;
-        }
-
+        potMutex.lock();
         leftPitch = L_Pitch.read();
         rightPitch = R_Pitch.read();
         leftRoll = L_Roll.read();
         rightRoll = R_Roll.read();
+        potMutex.unlock();
 
-        if ((BT1 == 1) || (BT2 == 1) || (BT3 == 1)){
-            buzzer = 1;
-        } else { buzzer = 0; }
-        printf("------------------------------\n\n");
+        printf("%c",char(27));  // ESC
+        printf("[2J");          // clear screen
+        printf("%c",char(27));  // ESC
+        printf("[H");           // cursor to home
+
+        printf("-----------------------\n");
         printf("Left Pitch: %f\n", leftPitch);
         printf("Right Pitch: %f\n", rightPitch);
         printf("Left Roll: %f\n", leftRoll);
-        printf("Right Roll: %f\n\n", rightRoll);
-        wait_us(1000000);
+        printf("Right Roll: %f\n", rightRoll);   
+        printf("-----------------------\n");
+
+        ThisThread::sleep_for(200ms);     
+
     }
+
 }
