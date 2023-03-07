33cdd0333f249d1705fc2b5bb3c8b8627e31859d
jjpendlebury <jack.j.pendlebury@students.plymouth.ac.uk>
2023-03-07 23:14:01 +0000

    LEDs and timing added

--- LiDAR Test/LiDar_Pico/LiDar_Pico.py
+++ LiDAR Test/LiDar_Pico/LiDar_Pico.py
@@ -1,13 +1,16 @@
 import pimoroni_i2c
 import breakout_vl53l5cx
+from machine import Pin
 import time
 #this is normally bad, but it's my module so meh
 #MAKE SURE THIS FILE IS PRESENT ON THE BOARD FIRST! IF IT CAN'T FIND IT, THAT'S WHY!
 from L_Proc import *
 
-#fun little number to find the average of a list
 
-
+#LED pins, for debugging only
+led1 = Pin(13,Pin.OUT)
+led2 = Pin(12, Pin.OUT)
+led3 = Pin(11, Pin.OUT)
 
 
 # The VL53L5CX requires a firmware blob to start up.
@@ -38,13 +41,29 @@ else:
 sensor.start_ranging()
 
 while True:
+    t_start = time.ticks_ms()
     if sensor.data_ready():
         # "data" is a namedtuple (attrtuple technically)
         # it includes average readings as "distance_avg" and "reflectance_avg"
         # plus a full 4x4 or 8x8 set of readings (as a 1d tuple) for both values.
         data = sensor.get_data()
-        diag_print(data, sensor_mode)
+        #diag_print(data, sensor_mode)
         print(centre_grid(data.distance, sensor_mode))
+        cent_reading = int(centre_grid_avg(centre_grid(data.distance, sensor_mode)))
         print("Average: {}".format(
-            centre_grid_avg(centre_grid(data.distance, sensor_mode))))
-        
+            cent_reading))
+        t_end2 = time.ticks_ms()
+        print("Sensing done in {}ms...".format(t_end2 - t_start))
+        ##now, do the LEDS
+        if cent_reading < 50:
+            led1.value(1)
+            led2.value(0)
+            led3.value(0)
+        elif (cent_reading > 50) and (cent_reading < 150):
+            led1.value(0)
+            led2.value(1)
+            led3.value(0)
+        else:
+            led1.value(0)
+            led2.value(0)
+            led3.value(1)
