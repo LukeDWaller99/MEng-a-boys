fce982e2fc539221011d5777625b8bf7050aeed6
jjpendlebury <jackjonpendlebury@gmail.com>
2023-03-14 17:32:19 +0000

    Change Scanner i2c pins

--- LiDAR Test/I2C_Scanner/i2c_scanner.py
+++ LiDAR Test/I2C_Scanner/i2c_scanner.py
@@ -1,6 +1,6 @@
 import machine
-sdaPIN=machine.Pin(0)
-sclPIN=machine.Pin(1)
+sdaPIN=machine.Pin(4)
+sclPIN=machine.Pin(5)
 i2c=machine.I2C(0,sda=sdaPIN, scl=sclPIN, freq=400000)
 devices = i2c.scan()
 if len(devices) != 0:
