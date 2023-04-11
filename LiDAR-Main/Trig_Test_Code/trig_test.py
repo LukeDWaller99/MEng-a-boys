aecc6ac9984ed56186e70ad78e8cd6951f1e6c96
Jack Pendlebury <jackjonpendlebury@gmail.com>
2023-04-11 16:30:14 +0100

    Trig Functions Written

--- LiDAR-Main/Trig_Test_Code/trig_test.py
+++ LiDAR-Main/Trig_Test_Code/trig_test.py
@@ -1,7 +1,10 @@
 from math import *
 
+def cos_law(b,c,A):
+    return sqrt(pow(b,2)+pow(c,2)-(2*b*c*(cos(radians(A)))))
 
-
+def sine_law(side1,side2,angle):
+    return asin((side1/side2)*sin(radians(angle)))
 L1=50 #c
 L2=120 #b
 A = 11.125
@@ -10,7 +13,7 @@ b=L2
 #cosine law for a
 a = sqrt(pow(b,2)+pow(c,2)-(2*b*c*(cos(radians(A)))))
 #sine law for b
-B = asin((L2/a)*sin(radians(11.125)))
+B = sine_law(L2,a,A)
 B = degrees(B)
 if (L2>L1):
     B = 180-B
