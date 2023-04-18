6033fe39e5f592e0a56c829c9ed5e9f883b73ca2
Jack Pendlebury <jackjonpendlebury@gmail.com>
2023-04-18 12:15:06 +0100

    Passing args to thread calls

--- LiDAR-Main/Code/MT_ex1.py
+++ LiDAR-Main/Code/MT_ex1.py
@@ -1,24 +1,25 @@
 #Basic multi thread example
  
-from time import sleep
+import time
 import _thread
- 
+from machine import Pin
+internal_led = Pin(25, Pin.OUT)
 def core0_thread():
     counter = 0
     while True:
         print(counter)
         counter += 2
-        sleep(1)
+        time.sleep(1)
  
  
-def core1_thread():
-    counter = 1
+def core1_thread(pin_in):
+    int_led = 0
     while True:
-        print(counter)
-        counter += 2
-        sleep(2)
+        int_led = int_led ^ 1
+        pin_in.value(int_led)
+        time.sleep(1)
  
  
-second_thread = _thread.start_new_thread(core1_thread, ())
+second_thread = _thread.start_new_thread(core1_thread, ([internal_led]))
  
 core0_thread()
