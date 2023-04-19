d9e8858fccbb9fa12570786010e53c5d01842cfc
Jack Pendlebury <jackjonpendlebury@gmail.com>
2023-04-19 11:01:05 +0100

    Revert "Working MT"
    This reverts commit 64dab72d7f878cc50869f35b2d0bf7becc06891e.

--- LiDAR-Test/LiDAR_Interrupts/LiDar_Pico_Tran_Bus.py
+++ LiDAR-Test/LiDAR_Interrupts/LiDar_Pico_Tran_Bus.py
@@ -49,6 +49,26 @@ def sensor2_callback(sensor):
     global int2_flag
     print("int2")
     int2_flag=1
+#boot all sensors
+t_sta = time.ticks_ms()
+for x in range(2):
+    bus.advance()
+    print("Starting up sensor...")
+    sensor = breakout_vl53l5cx.VL53L5CX(i2c)
+    if sensor_mode == 4:
+        sensor.set_resolution(breakout_vl53l5cx.RESOLUTION_4X4)
+    else:
+        sensor.set_resolution(breakout_vl53l5cx.RESOLUTION_8X8)
+    sensor.start_ranging()
+t_end = time.ticks_ms()
+print("Done in {}ms...".format(t_end - t_sta))
+gc.enable()
+
+#enable interrupts
+#interrupt1.irq(trigger=Pin.IRQ_RISING, handler=sensor1_callback)
+#interrupt2.irq(trigger=Pin.IRQ_RISING, handler=sensor2_callback)
+t_t1=time.ticks_ms()
+t_t2=time.ticks_ms()
 def f_sensing_loop(interrupt1,interrupt2,bus,sensor,led1,led2,led3):
     while True:
         global iterations,int_led
@@ -107,28 +127,62 @@ def f_sensing_loop(interrupt1,interrupt2,bus,sensor,led1,led2,led3):
             gc.collect()##clean up memory
             t_loop_end = time.ticks_ms()
             print("Loop done in {}ms...".format(t_loop_end - t_loop_sta))
-            
-#boot all sensors
-t_sta = time.ticks_ms()
-for x in range(2):
-    bus.advance()
-    print("Starting up sensor...")
-    sensor = breakout_vl53l5cx.VL53L5CX(i2c)
-    if sensor_mode == 4:
-        sensor.set_resolution(breakout_vl53l5cx.RESOLUTION_4X4)
-    else:
-        sensor.set_resolution(breakout_vl53l5cx.RESOLUTION_8X8)
-    sensor.start_ranging()
-t_end = time.ticks_ms()
-print("Done in {}ms...".format(t_end - t_sta))
-gc.enable()
-
-#enable interrupts
-#interrupt1.irq(trigger=Pin.IRQ_RISING, handler=sensor1_callback)
-#interrupt2.irq(trigger=Pin.IRQ_RISING, handler=sensor2_callback)
-t_t1=time.ticks_ms()
-t_t2=time.ticks_ms()
-
+# while True:
+#     t_loop_sta = time.ticks_ms()
+#     t_t1=time.ticks_ms()
+#     t_t2=time.ticks_ms()
+#     #bus.advance()
+#     #t_start = time.ticks_ms()
+#     while (interrupt1.get_flag() == 0 and interrupt2.get_flag() == 0): #wait until a flag goes high
+#         pass
+#     if (interrupt1.get_flag()==1):
+#         bus.enable(0)
+#         interrupt1.clear()
+#         t_t1s=time.ticks_ms()
+#         print("T1 interval {}ms...".format(t_t1s - t_t1))
+#         t_t1=time.ticks_ms()
+#     else:
+#         bus.enable(1)
+#         interrupt2.clear()
+#         t_t2s=time.ticks_ms()
+#         print("T2 interval {}ms...".format(t_t2s - t_t2))
+#         t_t2=time.ticks_ms()
+#     if sensor.data_ready():
+#         print("read")
+#         print(bus.current_enable)
+#         int_led = int_led ^ 1
+#         internal_led.value(int_led)
+#         # "data" is a namedtuple (attrtuple technically)
+#         # it includes average readings as "distance_avg" and "reflectance_avg"
+#         # plus a full 4x4 or 8x8 set of readings (as a 1d tuple) for both values.
+#         data = sensor.get_data()
+#         #diag_print(data, sensor_mode)
+#         #print(centre_grid(data.distance, sensor_mode))
+#         cent_reading = int(centre_grid_avg(centre_grid(data.distance, sensor_mode)))
+#         #print("Average: {}".format(
+#         #    cent_reading))
+#         #t_end2 = time.ticks_ms()
+#         #print("Sensing done in {}ms...".format(t_end2 - t_start))
+#         ##now, do the LEDS
+#         #cent_reading = 100
+#         if cent_reading < 50:
+#             led1.value(1)
+#             led2.value(0)
+#             led3.value(0)
+#         elif (cent_reading > 50) and (cent_reading < 250):
+#             led1.value(0)
+#             led2.value(1)
+#             led3.value(0)
+#         else:
+#             led1.value(0)
+#             led2.value(0)
+#             led3.value(1)
+#         iterations = iterations +1
+#         print(iterations)
+#         #micropython.mem_info()
+#         gc.collect()##clean up memory
+#         t_loop_end = time.ticks_ms()
+#         print("Loop done in {}ms...".format(t_loop_end - t_loop_sta))
 
 ##kick off thread running the sensing loop
 second_thread = _thread.start_new_thread(f_sensing_loop,[interrupt1,interrupt2,bus,sensor,led1,led2,led3])
