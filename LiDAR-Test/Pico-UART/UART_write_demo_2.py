848c2029c405115ed0d0f8c423642561dca62269
Jack Pendlebury <jackjonpendlebury@gmail.com>
2023-04-20 15:59:43 +0100

    fixed string issue using decode function

--- LiDAR-Test/Pico-UART/UART_write_demo_2.py
+++ LiDAR-Test/Pico-UART/UART_write_demo_2.py
@@ -1,7 +1,7 @@
 from machine import Pin,UART
 import time
 
-uart = UART(1, baudrate = 9600, tx=Pin(4), rx=Pin(5))
+uart = UART(0, baudrate = 9600, tx=Pin(0), rx=Pin(1))
 uart.init(bits=8, parity=None, stop=2)
 led = Pin ("LED",Pin.OUT)
 
@@ -14,6 +14,6 @@ while True:
         t_stop=time.ticks_cpu()
         print(t_stop-t_sta)
         print(data)
-        if data == b'Hello world':
+        if data.decode('UTF-8') == "Hello World":
             led.toggle()
     time.sleep(1)
