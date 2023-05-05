60c15fdef2c6091372cbaa08f82bedb1c8b180cc
gnarvo <58526792+gnarvo@users.noreply.github.com>
2023-05-05 11:18:38 +0100

    Init Push - LCD for L432KC

--- MBED_main/432_con/L432KC_Hardware.h
+++ MBED_main/432_con/L432KC_Hardware.h
@@ -4,7 +4,14 @@
 
 #define L4_USER_LED_GREEN PB_3
 
-
+//LCD
+#define LCD_RS D1
+#define LCD_E A6
+#define LCD_RW A7
+#define LCD_D4 D4
+#define LCD_D5 D5
+#define LCD_D6 D6
+#define LCD_D7 D7
 
 
 #endif
--- MBED_main/432_con/main.cpp
+++ MBED_main/432_con/main.cpp
@@ -1,14 +1,10 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
-
+#include "TextLCD.h"
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
+TextLCD::TextLCD();
 
 int main()
 {
