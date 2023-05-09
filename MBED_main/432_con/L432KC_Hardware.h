c57296ff8e86ab854b82d8617978eb66c259ea2c
LukeDWaller99 <lukedwaller99@gmail.com>
2023-05-09 14:31:14 +0100

    SPI Init

--- MBED_main/432_con/L432KC_Hardware.h
+++ MBED_main/432_con/L432KC_Hardware.h
@@ -6,12 +6,17 @@
 
 //LCD
 #define LCD_RS D1
-#define LCD_E D0
+#define LCD_E A6
 #define LCD_RW A7
-#define LCD_D4 D3
-#define LCD_D5 D4
-#define LCD_D6 D5
-#define LCD_D7 D6
+#define LCD_D4 D4
+#define LCD_D5 D5
+#define LCD_D6 D6
+#define LCD_D7 D7
 
+//SPI Pins - AS IN MANUAL
+#define MOSI PA_12 //D2
+#define MISO PA_11 //D10
+#define SCK PA_1 //A1
+#define SSEL A3//PA_4/A3 worked but taken
 
 #endif
--- MBED_main/432_con/main.cpp
+++ MBED_main/432_con/main.cpp
@@ -4,10 +4,25 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
+//SPI spi(MOSI, MISO, SCK);
+SPISlave spi(MOSI, MISO, SCK, SSEL);
 
 char foo[20] = "noah smells";
 int main()
-{
+{   
+    printf("Starting board...\n");
+    wait_us(500);
+    //spi business
+    int counter = 1;
+    spi.format(8,3); //8-bit mode
+    spi.frequency(1000000); //1MHz
+    //Test SPI responses
+    int reply = 99;
+    spi.reply(reply);
+    spi.reply(reply);
+
+    printf("SPI Device Response: %d\n", reply);
+
     LCD lcd(LCD_RS,LCD_RW,LCD_E,LCD_D4,LCD_D5,LCD_D6,LCD_D7);
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
@@ -15,6 +30,12 @@ int main()
     lcd.clear_LCD();
     lcd.put_char_array(foo);
     while (true) {
+        if (spi.receive()) {
+            int valueFromMaster = spi.read();
+            printf("Counter: %d . SPI Master Incoming: %d.\n", counter++, valueFromMaster);
+            spi.reply(++reply);
+            printf("Recieved: %d . Next Reply: %d \n", valueFromMaster, reply);
+        }
         led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
     }
