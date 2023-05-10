469d6d31a6b4558361756e06c5db8f7a586e6da5
gnarvo <58526792+gnarvo@users.noreply.github.com>
2023-05-10 16:07:37 +0100

    LCD

--- MBED_main/432_con/L432KC_Hardware.h
+++ MBED_main/432_con/L432KC_Hardware.h
@@ -4,33 +4,33 @@
 
 #define L4_USER_LED_GREEN PB_3
 
-//SPARE
+// //SPARE
 #define SP_D7 D7
 #define SP_D8 D8
 #define SP_A4 A4
 #define SP_A5 A5
 
 //LCD
-#define LCD_RS D1
-#define LCD_E A6
-#define LCD_RW A7
-#define LCD_D4 D4
-#define LCD_D5 D5
-#define LCD_D6 D6
-#define LCD_D7 D9
+#define LCD_RS D1 //PA_9
+#define LCD_E A6 //PA_7
+#define LCD_RW A7 //PA_2
+#define LCD_D4 D4 //PB_7
+#define LCD_D5 D5 //PB_6
+#define LCD_D6 D6 //PB_1
+#define LCD_D7 D9 //PA_8
 
 //D-PAD BUTTONS
-#define D_UP A0
-#define D_LEFT A2
-#define D_RIGHT D11
-#define D_DOWN A3
-#define D_SEL D12
-#define D_BACK D13
+#define D_UP A0 //PA_0
+#define D_LEFT A2 //PA_3
+//#define D_RIGHT D11 //PB_5 same as MOSI
+//#define D_DOWN A3 //PA_4 same as CS?
+//#define D_SEL D12 //PB_4 same as MISO
+//#define D_BACK D13 //PB_3 same as SCK
 
-//SPI Pins - AS IN MANUAL
-#define MOSI PA_12 //D2
-#define MISO PA_11 //D10
-#define SCK PA_1 //A1
-//#define SSEL A3//PA_4/A3 worked but taken
+// //SPI Pins - AS IN MANUAL
+// #define MOSI PA_12 //D2
+// #define MISO PA_11 //D10
+// #define SCK PA_1 //A1
+// //#define SSEL A3//PA_4/A3 worked but taken
 
 #endif
--- MBED_main/432_con/main.cpp
+++ MBED_main/432_con/main.cpp
@@ -2,44 +2,141 @@
 #include "LCD.h"
 #include "L432KC_Hardware.h"
 #include "ThisThread.h"
-// Blinking rate in milliseconds
+#include "InterruptIn.h"
+
 #define BLINKING_RATE     500ms
 
+InterruptIn UP(D_UP);
+//InterruptIn DOWN(D_DOWN);
+InterruptIn LEFT(D_LEFT);
+// InterruptIn RIGHT(D_RIGHT);
+// InterruptIn SEL(D_SEL);
+// InterruptIn BACK(D_BACK);
+Thread dpadMonitor;
+void dpadMonitorMethod();
+void dpadDetection();
+void up_IRQ();
+void down_IRQ();
+void left_IRQ();
+void right_IRQ();
+void sel_IRQ();
+void back_IRQ();
 
-// //SPI spi(MOSI, MISO, SCK);
-// //this one must be set as slave
-// SPISlave spi(MOSI, MISO, SCK, SP_D7);
+char foo [16]= "noah smells";
 
-// char foo[20] = "noah smells";
-// int main()
-// {   
-//     printf("Starting board...\n");
-//     wait_us(500);
-//     //spi business
-//     int counter = 1;
-//     spi.format(8,3); //8-bit mode
-//     spi.frequency(1000000); //1MHz
-//     //Test SPI responses
-//     int reply = 99;
-//     spi.reply(reply);
-//     spi.reply(reply);
+SPISlave myspi(PB_5, PB_4, PB_3, PA_4); // mosi, miso, sclk, nSS
+LCD LCD(LCD_RS, LCD_RW, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+int main() {
+    printf("L432 Controller Board Started\n");
+    
+    myspi.frequency(1000000);
+    myspi.format(8, 0);
+    myspi.reply(0x5A);
+    
 
-//     printf("SPI Device Response: %d\n", reply);
+    while(1) {
+         if (myspi.receive()) {
+             int data = myspi.read();
+             printf("Received %x2\n",data);
+             myspi.reply(data); // Read byte from master and transmit it at the next transaction
+         } 
+    }
+}
 
-//     LCD lcd(LCD_RS,LCD_RW,LCD_E,LCD_D4,LCD_D5,LCD_D6,LCD_D7);
-//     // Initialise the digital pin LED1 as an output
-//     DigitalOut led(LED1);
-//     lcd.put_char('a');
-//     lcd.clear_LCD();
-//     lcd.put_char_array(foo);
-//     while (true) {
-//         if (spi.receive()) {
-//             int valueFromMaster = spi.read();
-//             printf("Counter: %d . SPI Master Incoming: %d.\n", counter++, valueFromMaster);
-//             spi.reply(++reply);
-//             printf("Recieved: %d . Next Reply: %d \n", valueFromMaster, reply);
-//         }
-//         // led = !led;
-//         // ThisThread::sleep_for(BLINKING_RATE);
-//     }
-// }
+void up_IRQ(){
+    UP.rise(NULL);
+    wait_us(500);
+    if (UP == 1){
+        dpadMonitor.flags_set(1);
+    }
+    UP.rise(up_IRQ);
+}
+
+void down_IRQ(){
+    // DOWN.rise(NULL);
+    // wait_us(500);
+    // if (DOWN == 1){
+    //     dpadMonitor.flags_set(2);
+    // }
+    // DOWN.rise(down_IRQ);
+}
+
+void left_IRQ(){
+    LEFT.rise(NULL);
+    wait_us(500);
+    if (LEFT == 1){
+        dpadMonitor.flags_set(3);
+    }
+    LEFT.rise(left_IRQ);
+}
+
+void right_IRQ(){
+    // RIGHT.rise(NULL);
+    // wait_us(500);
+    // if (RIGHT == 1){
+    //     dpadMonitor.flags_set(4);
+    // }
+    // RIGHT.rise(right_IRQ);
+}
+
+void sel_IRQ(){
+    // SEL.rise(NULL);
+    // wait_us(500);
+    // if (SEL == 1){
+    //     dpadMonitor.flags_set(5);
+    // }
+    // SEL.rise(sel_IRQ);
+}
+
+void back_IRQ(){
+    // BACK.rise(NULL);
+    // wait_us(500);
+    // if (BACK == 1){
+    //     dpadMonitor.flags_set(6);
+    // }
+    // BACK.rise(back_IRQ);
+}
+
+void dpadDetection(){
+    UP.rise(up_IRQ);
+    // DOWN.rise(down_IRQ);
+    LEFT.rise(left_IRQ);
+    // RIGHT.rise(right_IRQ);
+    // SEL.rise(sel_IRQ);
+    // BACK.rise(back_IRQ);
+}
+
+void dpadMonitorMethod(){
+    printf("D-Pad Monitoring Thread Starte\n");
+    while (true) {
+        ThisThread::flags_wait_any(0x7fffffff, false);
+        int flag = ThisThread::flags_get();
+        ThisThread::flags_clear(0x7fffffff);
+
+        switch (flag) {
+            case 1:
+            printf("DPAD: UP\n");
+            break;
+
+            case 2:
+            printf("DPAD: DOWN\n");
+            break;
+
+            case 3:
+            printf("DPAD: LEFT\n");
+            break;
+
+            case 4:
+            printf("DPAD: RIGHT\n");
+            break;
+
+            case 5:
+            printf("DPAD: SEL\n");
+            break;
+
+            case 6:
+            printf("DPAD: BACK\n");
+            break;
+        }
+    }
+}
