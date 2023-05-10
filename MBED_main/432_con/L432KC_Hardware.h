6e11a20786a4915b2ba57f8a791980f0d97a50ac
gnarvo <58526792+gnarvo@users.noreply.github.com>
2023-05-10 11:33:36 +0100

    Spi Test 2

--- MBED_main/432_con/L432KC_Hardware.h
+++ MBED_main/432_con/L432KC_Hardware.h
@@ -4,6 +4,12 @@
 
 #define L4_USER_LED_GREEN PB_3
 
+//SPARE
+#define SP_D7 D7
+#define SP_D8 D8
+#define SP_A4 A4
+#define SP_A5 A5
+
 //LCD
 #define LCD_RS D1
 #define LCD_E A6
@@ -11,12 +17,20 @@
 #define LCD_D4 D4
 #define LCD_D5 D5
 #define LCD_D6 D6
-#define LCD_D7 D7
+#define LCD_D7 D9
+
+//D-PAD BUTTONS
+#define D_UP A0
+#define D_LEFT A2
+#define D_RIGHT D11
+#define D_DOWN A3
+#define D_SEL D12
+#define D_BACK D13
 
 //SPI Pins - AS IN MANUAL
 #define MOSI PA_12 //D2
 #define MISO PA_11 //D10
 #define SCK PA_1 //A1
-#define SSEL A3//PA_4/A3 worked but taken
+//#define SSEL A3//PA_4/A3 worked but taken
 
 #endif
--- MBED_main/432_con/main.cpp
+++ MBED_main/432_con/main.cpp
@@ -5,40 +5,41 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
-//SPI spi(MOSI, MISO, SCK);
-//this one must be set as slave
-SPISlave spi(MOSI, MISO, SCK, SSEL);
 
-char foo[20] = "noah smells";
-int main()
-{   
-    printf("Starting board...\n");
-    wait_us(500);
-    //spi business
-    int counter = 1;
-    spi.format(8,3); //8-bit mode
-    spi.frequency(1000000); //1MHz
-    //Test SPI responses
-    int reply = 99;
-    spi.reply(reply);
-    spi.reply(reply);
+// //SPI spi(MOSI, MISO, SCK);
+// //this one must be set as slave
+// SPISlave spi(MOSI, MISO, SCK, SP_D7);
 
-    printf("SPI Device Response: %d\n", reply);
+// char foo[20] = "noah smells";
+// int main()
+// {   
+//     printf("Starting board...\n");
+//     wait_us(500);
+//     //spi business
+//     int counter = 1;
+//     spi.format(8,3); //8-bit mode
+//     spi.frequency(1000000); //1MHz
+//     //Test SPI responses
+//     int reply = 99;
+//     spi.reply(reply);
+//     spi.reply(reply);
 
-    LCD lcd(LCD_RS,LCD_RW,LCD_E,LCD_D4,LCD_D5,LCD_D6,LCD_D7);
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
-    lcd.put_char('a');
-    lcd.clear_LCD();
-    lcd.put_char_array(foo);
-    while (true) {
-        if (spi.receive()) {
-            int valueFromMaster = spi.read();
-            printf("Counter: %d . SPI Master Incoming: %d.\n", counter++, valueFromMaster);
-            spi.reply(++reply);
-            printf("Recieved: %d . Next Reply: %d \n", valueFromMaster, reply);
-        }
-        // led = !led;
-        // ThisThread::sleep_for(BLINKING_RATE);
-    }
-}
+//     printf("SPI Device Response: %d\n", reply);
+
+//     LCD lcd(LCD_RS,LCD_RW,LCD_E,LCD_D4,LCD_D5,LCD_D6,LCD_D7);
+//     // Initialise the digital pin LED1 as an output
+//     DigitalOut led(LED1);
+//     lcd.put_char('a');
+//     lcd.clear_LCD();
+//     lcd.put_char_array(foo);
+//     while (true) {
+//         if (spi.receive()) {
+//             int valueFromMaster = spi.read();
+//             printf("Counter: %d . SPI Master Incoming: %d.\n", counter++, valueFromMaster);
+//             spi.reply(++reply);
+//             printf("Recieved: %d . Next Reply: %d \n", valueFromMaster, reply);
+//         }
+//         // led = !led;
+//         // ThisThread::sleep_for(BLINKING_RATE);
+//     }
+// }
